9ddfbd29236f7541beec71c0c9395f08b1d29521
jzmaddock <john@johnmaddock.co.uk>
2024-04-19 18:25:36 +0100

    Adjust test script again.

--- test/module/msvc_test.bat
+++ test/module/msvc_test.bat
@@ -1,8 +1,8 @@
 REM Basic command line to build everything with msvc:
 
-del *.obj
-del *.exe
-del *.lib
+rem del *.obj
+rem del *.exe
+rem del *.lib
 
 cl /std:c++latest /EHsc /nologo /W4 /c "%VCToolsInstallDir%\modules\std.ixx"  || exit 1
 cl /std:c++latest /EHsc /nologo /W4 /c /interface /I ..\..\..\.. ..\..\module\regex.cxx || exit 1
@@ -10,13 +10,13 @@ cl /std:c++latest /EHsc /nologo /W4 /c /I ..\..\..\.. ..\..\module\*.cpp || exit
 
 lib *.obj /OUT:regex.lib || exit 1
 
-time < nul
+rem time < nul
 
 for %%f in (*.cpp) do (
    cl /std:c++latest /EHsc /nologo /W4 /I ..\..\..\.. %%f regex.lib || exit 1
 )
 
-time < nul
+rem time < nul
 
 rem uncomment this section to test/time non-module build:
 rem for %%f in (*.cpp) do (
